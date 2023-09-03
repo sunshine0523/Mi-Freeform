15de9dd5a14c3d0a21621c0040ccc32051a3fd2e
KindBrave <1773747161@qq.com>
2023-09-03 12:34:24 +0800

    Fix can not touch permission dialog bug

--- freeform-server/src/main/java/io/sunshine0523/freeform/ui/sidebar/SideBarWindow.kt
+++ freeform-server/src/main/java/io/sunshine0523/freeform/ui/sidebar/SideBarWindow.kt
@@ -51,7 +51,7 @@ class SideBarWindow(
             rightView.setBackgroundColor(Color.TRANSPARENT)
             SideBarTouchListener(this)
             leftWindowParams.apply {
-                type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                type = 2017
                 width = 50
                 height = screenHeight / 5
                 x = -screenWidth / 2
@@ -60,7 +60,7 @@ class SideBarWindow(
                 format = PixelFormat.RGBA_8888
             }
             rightWindowParams.apply {
-                type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                type = 2017
                 width = 50
                 height = screenHeight / 5
                 x = screenWidth / 2
